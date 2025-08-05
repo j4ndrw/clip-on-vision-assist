be841efa015b6c16be859a4132889f70bfde88e1
j4ndrw <murjan_andreialin@yahoo.com>
2025-08-05 16:45:28 +0300

    COVA-2 feat(pi-server/control-center): added wifi routes to the control center server

--- pi-server/control_center_server.py
+++ pi-server/control_center_server.py
@@ -6,8 +6,12 @@ from flask_pydantic import validate
 
 from src.control_center.models.bluetooth.endpoint.get_bluetooth_devices import GetBluetoothDevicesResponse
 from src.control_center.models.bluetooth.endpoint.connect_bluetooth_headphones import ConnectBluetoothHeadphonesRequest
+from src.control_center.models.wifi.endpoint.scan_networks import ScanNetworksResponse
+from src.control_center.models.wifi.endpoint.connect_to_network import ConnectToNetworkRequest
 from src.control_center.services.bluetooth.get_bluetooth_devices import get_bluetooth_devices
 from src.control_center.services.bluetooth.connect_bluetooth_headphones import connect_bluetooth_headphones
+from src.control_center.services.wifi.scan_networks import scan_networks
+from src.control_center.services.wifi.connect_to_network import connect_to_network
 
 loop = asyncio.get_event_loop()
 app = Flask(__name__)
@@ -28,6 +32,23 @@ def connect_bluetooth_headphones_route():
     if err is not None:
         return Response(response=err.as_json(), status=HTTPStatus.BAD_REQUEST)
     return Response(status=HTTPStatus.OK)
+
+@app.get("/api/wifi")
+@validate()
+def scan_networks_route():
+    wifi_networks, err = scan_networks()
+    if err is not None:
+        return Response(response=err.as_json(), status=HTTPStatus.CONFLICT)
+    return ScanNetworksResponse(wifi_networks=wifi_networks).as_json()
+
+@app.post("/api/wifi")
+@validate(body=ConnectToNetworkRequest, response_by_alias=True)
+def connect_to_network_route():
+    body = ConnectToNetworkRequest(**request.json or {})
+    err = connect_to_network(ssid=body.ssid, password=body.password)
+    if err is not None:
+        return Response(response=err.as_json(), status=HTTPStatus.BAD_REQUEST)
+    return Response(status=200)
 
 if __name__ == "__main__":
     app.run(port=42068) # disappointing
