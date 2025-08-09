392c3eff51440ce836253611735b1f615c74b72e
j4ndrw <murjan_andreialin@yahoo.com>
2025-08-09 17:50:11 +0300

    COVA-3 fix(pi-server): fixed systemd create-hotspot-ap service so that it creates the hotspot and amends the config files when pi is powered on

--- pi-server/post-deploy.sh
+++ pi-server/post-deploy.sh
@@ -2,13 +2,16 @@
 
 set -xe
 
-DEST_DIR="$HOME/projects/clip-on-vision-assist-client"
+DEST_DIR="$HOME/projects/clip-on-vision-assist-satellite"
 SCRIPTS_DIR="$DEST_DIR/scripts"
 
-LINKED_EXECUTABLE="/usr/local/bin/clip-on-vision-assist-client"
+CLIENT_LINKED_EXECUTABLE="/usr/local/bin/clip-on-vision-assist-client"
+CONTROL_CENTER_SERVER_LINKED_EXECUTABLE="/usr/local/bin/clip-on-vision-assist-control-center-server"
 
 SYSTEMD_COVA_CLIENT_SERVICE_NAME="clip-on-vision-assist-client"
 SYSTEMD_COVA_CLIENT_SERVICE="$SYSTEMD_COVA_CLIENT_SERVICE_NAME.service"
+SYSTEMD_COVA_CONTROL_CENTER_SERVER_SERVICE_NAME="clip-on-vision-assist-control-center-server"
+SYSTEMD_COVA_CONTROL_CENTER_SERVER_SERVICE="$SYSTEMD_COVA_CONTROL_CENTER_SERVER_SERVICE_NAME.service"
 USER_SYSTEMD_DIR="$HOME/.config/systemd/user"
 
 SYSTEMD_CREATE_HOTSPOT_SERVICE_NAME="create-hotspot-ap"
@@ -19,23 +22,34 @@ function enable_systemd_services()
 {
     sudo systemctl daemon-reload
     systemctl --user enable $SYSTEMD_COVA_CLIENT_SERVICE
+    systemctl --user enable $SYSTEMD_COVA_CONTROL_CENTER_SERVER_SERVICE
     sudo systemctl enable $SYSTEMD_CREATE_HOTSPOT_SERVICE
 }
 
 function allow_executables()
 {
-    chmod +x $DEST_DIR/run-on-pi.sh
+    chmod +x $DEST_DIR/run-on-pi__client.sh
+    chmod +x $DEST_DIR/run-on-pi__control-center-server.sh
     chmod +x $SCRIPTS_DIR/*
 }
 
+sed -i "s#<DEST_DIR>#$DEST_DIR#g" $DEST_DIR/scripts/setup-hotspot.sh
+sed -i "s#<DEST_DIR>#$DEST_DIR#g" $DEST_DIR/firmware/etc/systemd/system/$SYSTEMD_CREATE_HOTSPOT_SERVICE
+
 allow_executables
-sudo ln -sf $DEST_DIR/run-on-pi.sh $LINKED_EXECUTABLE
+sudo ln -sf $DEST_DIR/run-on-pi__client.sh $CLIENT_LINKED_EXECUTABLE
+sudo ln -sf $DEST_DIR/run-on-pi__control-center-server.sh $CONTROL_CENTER_SERVER_LINKED_EXECUTABLE
 
 $SCRIPTS_DIR/setup.sh
 
 mkdir -p $USER_SYSTEMD_DIR
+
 sudo cp $DEST_DIR/firmware/user/.config/systemd/user/$SYSTEMD_COVA_CLIENT_SERVICE $USER_SYSTEMD_DIR/$SYSTEMD_COVA_CLIENT_SERVICE
 sudo chmod 640 $USER_SYSTEMD_DIR/$SYSTEMD_COVA_CLIENT_SERVICE
+
+sudo cp $DEST_DIR/firmware/user/.config/systemd/user/$SYSTEMD_COVA_CONTROL_CENTER_SERVER_SERVICE $USER_SYSTEMD_DIR/$SYSTEMD_COVA_CONTROL_CENTER_SERVER_SERVICE
+sudo chmod 640 $USER_SYSTEMD_DIR/$SYSTEMD_COVA_CONTROL_CENTER_SERVER_SERVICE
+
 sudo chown $USER:$USER -R $USER_SYSTEMD_DIR
 sudo ln -sf /usr/lib/systemd/user/default.target $USER_SYSTEMD_DIR/default.target
 
